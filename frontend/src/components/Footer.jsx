5826e5286638ffe2566ee1357d9ab124e1a92d76
ch <tndevfactory@gmail.com>
2023-12-09 18:03:30 +0100

    footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { Col, Row } from "antd";
+
 const footerStyle = {
   backgroundColor: "#444",
   color: "white",
@@ -9,7 +11,59 @@ const footerStyle = {
 const Footer = () => {
   return (
     <>
-      <div style={footerStyle}>footer systeme de liste</div>
+      <div style={footerStyle}>
+        <Row gutter={24}>
+          {[1, 2, 3, 4].map((item, i) => (
+            <Col
+              key={i}
+              style={{
+                color: "white",
+                padding: "10px",
+
+                display: "flex",
+                justifyContent: "center",
+              }}
+              xs={12}
+              xl={6}
+            >
+              <div style={{ padding: "5px" }}>
+                <h4 style={{ marginBottom: "10px" }}>Membre services</h4>
+                <a
+                  href=""
+                  style={{
+                    display: "block",
+                    color: "white",
+                    marginBottom: "5px",
+                  }}
+                >
+                  Voir profil
+                </a>
+
+                <a
+                  href=""
+                  style={{
+                    display: "block",
+                    color: "white",
+                    marginBottom: "5px",
+                  }}
+                >
+                  Voir profil
+                </a>
+                <a
+                  href=""
+                  style={{
+                    display: "block",
+                    color: "white",
+                    marginBottom: "5px",
+                  }}
+                >
+                  Voir profil
+                </a>
+              </div>
+            </Col>
+          ))}
+        </Row>
+      </div>
 
       <div
         style={{
